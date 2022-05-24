78bdcbbc564d9595231e18d0b6bea6ac39703f90
Andrew Tait <Andrew.Tait@decisionmechanics.com>
2022-05-24 14:40:16 +0100

    Update configuration

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="betting",
-    version="0.0.3",
+    version="0.0.4",
     description="Utility library for converting between betting odds",
     long_description=README,
     long_description_content_type="text/markdown",
